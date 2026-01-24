603b43659822593501b93b98d132e3da8f60d182
NhungNhung02 <nhunglehong06022005@gmail.com>
2026-01-24 12:04:45 +0700

    Btap2_Button_Reactjs

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -1,13 +1,11 @@
-import ProductCard from "./components/Btap1/ProductCard/ProductCard";
+import Button from "./components/Btap2/Button/Button";
 
 function App() {
   return (
-    <div style={{ padding: "40px" }}>
-      <ProductCard
-        image="https://picsum.photos/300/200"
-        name="Nike Air Force 1"
-        price={120}
-      />
+    <div style={{ padding: "40px", display: "flex", gap: "12px" }}>
+      <Button type="primary" text="Save" />
+      <Button type="danger" text="Delete" />
+      <Button type="success" text="Submit" />
     </div>
   );
 }
